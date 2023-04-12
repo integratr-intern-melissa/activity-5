0d56a6ee886a25d6d37c15076aa80c4079c470b1
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-12 18:05:48 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const input = document.getElementById("add");
+    function Addingbox() {
+    let add = document.createElement("add");
+    newListItem.textContent = 'add';
+    input.appendChild(add);
+    }
